f85afb10e078e2ac28c86b2775c2dab9360e2a66
FelipeAkel <felipe.akel01@gmail.com>
2023-12-05 21:04:18 -0300

    Estrutura das Pastas e Template Bootstrap

---
+++ resources/views/dasboard.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.template')
+
+@section('titulo', 'Dasboard')
+
+@section('conteudo')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Dashboard</h1>
+    </div>
+    
+    <canvas class="my-4 w-100" id="myChart" width="900" height="180"></canvas>
+
+    <h2>Section title</h2>
+    <div class="table-responsive small">
+        <table class="table table-striped table-sm">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                    <th scope="col">Header</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1,014</td>
+                    <td>dashboard</td>
+                    <td>illustrative</td>
+                    <td>rich</td>
+                    <td>data</td>
+                </tr>
+                <tr>
+                    <td>1,015</td>
+                    <td>random</td>
+                    <td>tabular</td>
+                    <td>information</td>
+                    <td>text</td>
+                </tr>
+                <tr>
+                    <td>1,014</td>
+                    <td>dashboard</td>
+                    <td>illustrative</td>
+                    <td>rich</td>
+                    <td>data</td>
+                </tr>
+                <tr>
+                    <td>1,015</td>
+                    <td>random</td>
+                    <td>tabular</td>
+                    <td>information</td>
+                    <td>text</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+@endsection
---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="pt-br" data-bs-theme="auto">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Curso Mentoria Laravel 10 - Udemy">
+    <meta name="author" content="Felipe Akel Carvalho Florentino">
+    <title>SG - @yield('titulo')</title>
+
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet" >
+    <link href="{{ asset('css/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/dashboard.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="{{ asset('img/icones/apple-touch-icon.png') }}" sizes="180x180">
+    <link rel="icon" href="{{ asset('img/icones/favicon-32x32.png') }}" sizes="32x32" type="image/png">
+    <link rel="icon" href="{{ asset('img/icones/favicon-16x16.png') }}" sizes="16x16" type="image/png">
+    <link rel="manifest" href="{{ asset('img/icones/manifest.json') }}">
+    <link rel="mask-icon" href="{{ asset('img/icones/safari-pinned-tab.svg') }}" color="#712cf9">
+    <link rel="icon" href="{{ asset('img/icones/favicon.ico') }}">
+    <meta name="theme-color" content="#712cf9">
+
+</head>
+
+<body>
+
+    @include('layouts.component.icon')
+
+    <header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
+        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6 text-white" href="#">Super Gestão - SG</a>
+        <ul class="navbar-nav flex-row d-md-none">
+            <li class="nav-item text-nowrap">
+                <button class="nav-link px-3 text-white" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <svg class="bi">
+                        <use xlink:href="#list" />
+                    </svg>
+                </button>
+            </li>
+        </ul>
+    </header>
+
+    <div class="container-fluid">
+        <div class="row">
+            
+            @include('layouts.component.navegacao')
+
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                
+                @yield('conteudo')
+                
+            </main>
+        </div>
+    </div>
+
+    <script src="{{ asset('js/bootstrap.bundle.js') }}" ></script>
+    <script src="{{ asset('js/color-modes.js') }}"></script>
+    <script src="{{ asset('js/chart.umd.js') }}" ></script>
+    <script src="{{ asset('js/dashboard.js') }}"></script>
+</body>
+
+</html>
